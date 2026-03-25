7a03783616c69523d7e47ab0c81c69d664f5df8a
ky381011 <ky381011git@gmail.com>
2026-03-25 22:06:15 +0900

    useStateのインポートをコメントアウトし、ボタンを追加

--- practice/tailwind/basic/src/App.jsx
+++ practice/tailwind/basic/src/App.jsx
@@ -1,11 +1,12 @@
 import "./style.css";
-import { useState } from "react";
+// import { useState } from "react";
 
 export function App() {
   return (
     <>
       <div>
         <h1 class="text-5xl font-extrabold tracking-tight">サンプル</h1>
+        <button class="bg-blue-100 text-white">ボタン</button>
       </div>
     </>
   );
