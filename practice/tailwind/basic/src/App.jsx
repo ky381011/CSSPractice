ca5982ce89609c01dd486a66a7e0ba9c007ab8fc
ky381011 <ky381011git@gmail.com>
2026-03-25 22:12:35 +0900

    ボタンのスタイルを更新し、背景色とパディングを追加

--- practice/tailwind/basic/src/App.jsx
+++ practice/tailwind/basic/src/App.jsx
@@ -6,7 +6,7 @@ export function App() {
     <>
       <div>
         <h1 class="text-5xl font-extrabold tracking-tight">サンプル</h1>
-        <button class="bg-blue-100 text-white">ボタン</button>
+        <button class="bg-blue-600 text-white px-6 py-3 mt-6">ボタン</button>
       </div>
     </>
   );
