267ded1af823acbb7e937e1d82917f7bcec5eed2
ky381011 <ky381011git@gmail.com>
2026-03-26 21:30:33 +0900

    コンテナクラスを追加してレイアウトを整えました

--- practice/tailwind/basic/src/App.jsx
+++ practice/tailwind/basic/src/App.jsx
@@ -4,7 +4,7 @@ import "./style.css";
 export function App() {
   return (
     <>
-      <div>
+      <div class="container mx-auto px-4 py-16">
         <h1 class="text-5xl font-extrabold tracking-tight">サンプル</h1>
         <button class="bg-blue-600 text-white px-6 py-3 mt-6 rounded-lg shadow-md hover:bg-blue-700 transition duration-700">ボタン</button>
       </div>
