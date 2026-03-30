7ac93c6f303a17493d6262046aedcc866a5438b1
ky381011 <ky381011git@gmail.com>
2026-03-30 21:39:51 +0900

    feat: App.tsxの構造を更新し、TailwindCSSのスタイルを適用

--- practice/tailwind/basic-02/src/App.tsx
+++ practice/tailwind/basic-02/src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css'
-
 function App() {
   return (
     <>
       <div>
-        <h1 className="text-7xl font-bold">TailwindCSS入門</h1>
+        <div className='border border-blue-400 p-16 my-10 mx-10'>
+          <h1 className="text-7xl font-bold">TailwindCSS入門</h1>
+        </div>
       </div>
     </>
   )
