645ecf64c953a3fbc14950bc99b32feaf1498b6c
ky381011 <ky381011git@gmail.com>
2026-03-30 21:50:22 +0900

    feat: App.tsxにTailwindCSSスタイルの新しいセクションを追加

--- practice/tailwind/basic-02/src/App.tsx
+++ practice/tailwind/basic-02/src/App.tsx
@@ -3,6 +3,9 @@ function App() {
     <>
       <div>
         <div className='border border-blue-400 p-16 my-10 mx-10'>
+          <h1 className="lg:text-7xl font-bold sm:text-red-400 md:text-blue-400 lg:text-pink-400 xl:text-green-400">TailwindCSS入門</h1>
+        </div>
+        <div className='border border-blue-400 p-16 my-10 mx-10'>
           <h1 className="text-7xl font-bold">TailwindCSS入門</h1>
         </div>
       </div>
