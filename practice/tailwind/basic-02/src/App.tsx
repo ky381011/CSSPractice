5f6651994ac4881b8f2812acbd41b160c999fa51
ky381011 <ky381011git@gmail.com>
2026-03-31 21:26:56 +0900

    feat: App.tsxに新しいボタンスタイルを追加するためにApp.cssを作成

--- practice/tailwind/basic-02/src/App.tsx
+++ practice/tailwind/basic-02/src/App.tsx
@@ -1,3 +1,5 @@
+import './App.css'
+
 function App() {
   return (
     <>
@@ -8,6 +10,7 @@ function App() {
         <div className='border border-blue-400 p-16 my-10 mx-10'>
           <h1 className="text-7xl font-bold">TailwindCSS入門</h1>
         </div>
+        <button className="easy-button">ボタン</button>
       </div>
     </>
   )
