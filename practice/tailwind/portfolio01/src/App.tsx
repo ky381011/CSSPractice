7e58a2556f974b2f54528873ff433c19b48d97bc
ky381011 <ky381011git@gmail.com>
2026-04-05 22:15:36 +0900

    feat: App.tsxからHello Worldを削除し、ナビゲーションバーを追加

--- practice/tailwind/portfolio01/src/App.tsx
+++ practice/tailwind/portfolio01/src/App.tsx
@@ -4,7 +4,19 @@ function App() {
   return (
     <>
       <div className='min-h-screen font-poppins bg-body text-white'>
-        <h1 >Hello World!!</h1>
+        {/* ナビゲーションバー */}
+        {/* ヘッダー部分 */}
+        <div className='py-6'> 
+          <div>
+            <div>SAMPLE.com</div>
+            <div>
+              <a href="#home">ホーム</a>
+              <a href="#home">ポートフォリオ</a>
+              <a href="#home">お客様</a>
+              <a href="#home"><button>お問い合わせ</button></a>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
