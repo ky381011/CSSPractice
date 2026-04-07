e9833460feaf7976b74a87d93b94b8b665fb9914
ky381011 <ky381011git@gmail.com>
2026-04-07 23:17:30 +0900

    style: ナビゲーションバーのリンクを整形し、可読性を向上

--- practice/tailwind/portfolio01/src/App.tsx
+++ practice/tailwind/portfolio01/src/App.tsx
@@ -10,10 +10,22 @@ function App() {
           <div className='container mx-auto flex justify-between items-center px-8 md:px-14 lg:px-24 w-full'>
             <div className='text-lg font-bold'>SAMPLE.com</div>
             <div className='space-x-12'>
-              <a href="#home" className='hover:text-selected-text transition-all duration-300'>ホーム</a>
-              <a href="#portfolio" className='hover:text-selected-text transition-all duration-300'>ポートフォリオ</a>
-              <a href="#clients" className='hover:text-selected-text transition-all duration-300'>お客様</a>
-              <a href="#contact" className='hover:text-selected-text transition-all duration-300'><button>お問い合わせ</button></a>
+              <a
+                href="#home"
+                className='hover:text-selected-text transition-all duration-300'
+              >ホーム</a>
+              <a
+                href="#portfolio"
+                className='hover:text-selected-text transition-all duration-300'
+              >ポートフォリオ</a>
+              <a
+                href="#clients"
+                className='hover:text-selected-text transition-all duration-300'
+              >お客様</a>
+              <a
+                href="#contact"
+                className='hover:text-selected-text transition-all duration-300'
+              ><button>お問い合わせ</button></a>
             </div>
           </div>
         </div>
