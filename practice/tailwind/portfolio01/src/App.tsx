d5e66a342c35e11e72102735d6b9f65ada4bc6e2
ky381011 <ky381011git@gmail.com>
2026-04-07 23:15:52 +0900

    feat: ナビゲーションバーの構造を整理し、スタイルを統一

--- practice/tailwind/portfolio01/src/App.tsx
+++ practice/tailwind/portfolio01/src/App.tsx
@@ -7,13 +7,13 @@ function App() {
         {/* ナビゲーションバー */}
         {/* ヘッダー部分 */}
         <div className='py-6'> 
-          <div>
-            <div>SAMPLE.com</div>
-            <div className='container mx-auto flex justify-between items-center px-8 md:px-14 lg:px-24 w-full'>
-              <a href="#home">ホーム</a>
-              <a href="#portfolio">ポートフォリオ</a>
-              <a href="#clients">お客様</a>
-              <a href="#contact"><button>お問い合わせ</button></a>
+          <div className='container mx-auto flex justify-between items-center px-8 md:px-14 lg:px-24 w-full'>
+            <div className='text-lg font-bold'>SAMPLE.com</div>
+            <div className='space-x-12'>
+              <a href="#home" className='hover:text-selected-text transition-all duration-300'>ホーム</a>
+              <a href="#portfolio" className='hover:text-selected-text transition-all duration-300'>ポートフォリオ</a>
+              <a href="#clients" className='hover:text-selected-text transition-all duration-300'>お客様</a>
+              <a href="#contact" className='hover:text-selected-text transition-all duration-300'><button>お問い合わせ</button></a>
             </div>
           </div>
         </div>
