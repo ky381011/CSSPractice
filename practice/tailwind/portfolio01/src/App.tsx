59085e5815c160b2be10cc8f7c2abc85301f03ae
ky381011 <ky381011git@gmail.com>
2026-04-09 23:08:51 +0900

    feat: ヒーローセクションにボタンを追加し、コンテンツを強化

--- practice/tailwind/portfolio01/src/App.tsx
+++ practice/tailwind/portfolio01/src/App.tsx
@@ -35,7 +35,16 @@ function App() {
         {/* ヒーローセクション */}
         <div id="home" className='container mt-16 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full'>
           <div className='flex flex-wrap md:flex-nowrap'>
-
+            <div className='md:my-36 lg:ml-20'>
+              <h1 className='font-bold text-5xl text-center md:text-left lg:text-7xl md:text-6xl'>
+                TailwindCSSで <br />
+                美しいサイトを <br />
+                作ります
+              </h1>
+              <button className='px-6 py-4 bg-theme font-bold rounded-lg hover:bg-purple-600 transition-all duration-300 mt-10'>
+                <span><i className="fa-solid fa-rocket mr-1"></i>もっと見る</span>
+              </button>
+            </div>
           </div>
         </div>
       </div>
