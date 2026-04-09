7e10d5cdb0aa06e40ee0cbd1d7b8c5dd4bc95e64
ky381011 <ky381011git@gmail.com>
2026-04-09 22:54:50 +0900

    feat: フォントアイコンを追加し、ナビゲーションバーのレスポンシブデザインを改善

--- practice/tailwind/portfolio01/src/App.tsx
+++ practice/tailwind/portfolio01/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <div className='py-6'> 
           <div className='container mx-auto flex justify-between items-center px-8 md:px-14 lg:px-24 w-full'>
             <div className='text-lg font-bold'>SAMPLE.com</div>
-            <div className='space-x-12'>
+            <div className='space-x-12 hidden md:flex items-center'>
               <a
                 href="#home"
                 className='hover:text-selected-text transition-all duration-300'
@@ -26,6 +26,9 @@ function App() {
                 href="#contact"
                 className='hover:text-selected-text transition-all duration-300'
               ><button className='px-6 py-2 bg-theme font-bold rounded-lg hover:bg-purple-600 transition-all duration-300'>お問い合わせ</button></a>
+            </div>
+            <div className='md:hidden'>
+              <i className="fa-solid fa-bars"></i>
             </div>
           </div>
         </div>
