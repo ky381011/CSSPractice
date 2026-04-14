fbc1a657b81e105d9b3f03f9542732f7c7832487
ky381011 <ky381011git@gmail.com>
2026-04-14 23:14:12 +0900

    feat: スキルスタックのJavascriptセクションにホバー効果を追加し、説明文のスタイルを改善

--- practice/tailwind/portfolio01/src/App.tsx
+++ practice/tailwind/portfolio01/src/App.tsx
@@ -77,19 +77,19 @@ function App() {
               私は以下のプログラミングをx年以上取り扱ってきました。
             </p>
             <div className='my-16'>
-              <div className='w-full border border-purple-300 p-16 lg:px-32 lg:py-20 flex justify-center lg:justify-start flex-wrap space-x-32'>
+              <div className='w-full border border-purple-300 p-16 lg:px-32 lg:py-20 flex justify-center lg:justify-start flex-wrap lg:space-x-32 hover:border-yellow-300 transition-all duration-300 cursor-pointer'>
                 {/* 言語のロゴ */}
                 <div className='mb-6 lg:mb-0 self-center'>
                   <i className="fa-brands fa-square-js text-9xl text-yellow-300"></i>
                 </div>
                 {/* スキルの説明 */}
-                <div>
+                <div className='text-center flex flex-wrap justify-center lg:text-left lg:block'>
                   <h3 className='text-3xl font-semibold'>Javascript</h3>
                   <div className='flex flex-wrap w-full justify-center gap-3 lg:w-auto lg:justify-start mt-6 mb-8'>
                     <div className='badge'>ウェブサイト制作</div>
                     <div className='badge'>フロントエンド開発</div>
                   </div>
-                  <p>Javascriptエンジニア。普段はReactを使って...</p>
+                  <p className='text-secondary'>Javascriptエンジニア。普段はReactを使って...</p>
                 </div>
               </div>
             </div>
