06d66556afe3c99f0c20855a19d60063d1b168ac
ky381011 <ky381011git@gmail.com>
2026-04-15 23:13:01 +0900

    feat: スキルスタックにPythonとHTML/CSSのセクションを追加し、説明文を強化

--- practice/tailwind/portfolio01/src/App.tsx
+++ practice/tailwind/portfolio01/src/App.tsx
@@ -93,6 +93,40 @@ function App() {
                 </div>
               </div>
             </div>
+            <div className='my-16'>
+              <div className='w-full border border-purple-300 p-16 lg:px-32 lg:py-20 flex justify-center lg:justify-start flex-wrap lg:space-x-32 hover:border-blue-300 transition-all duration-300 cursor-pointer'>
+                {/* 言語のロゴ */}
+                <div className='mb-6 lg:mb-0 self-center'>
+                  <i className="fa-brands fa-python text-9xl text-blue-300"></i>
+                </div>
+                {/* スキルの説明 */}
+                <div className='text-center flex flex-wrap justify-center lg:text-left lg:block'>
+                  <h3 className='text-3xl font-semibold'>Python</h3>
+                  <div className='flex flex-wrap w-full justify-center gap-3 lg:w-auto lg:justify-start mt-6 mb-8'>
+                    <div className='badge'>AI/DX</div>
+                    <div className='badge'>データサイエンス</div>
+                  </div>
+                  <p className='text-secondary'>AI開発をメインに...</p>
+                </div>
+              </div>
+            </div>
+            <div className='my-16'>
+              <div className='w-full border border-purple-300 p-16 lg:px-32 lg:py-20 flex justify-center lg:justify-start flex-wrap lg:space-x-32 hover:border-orange-400 transition-all duration-300 cursor-pointer'>
+                {/* 言語のロゴ */}
+                <div className='mb-6 lg:mb-0 self-center'>
+                  <i className="fa-brands fa-html5 text-9xl text-orange-400"></i>
+                </div>
+                {/* スキルの説明 */}
+                <div className='text-center flex flex-wrap justify-center lg:text-left lg:block'>
+                  <h3 className='text-3xl font-semibold'>HTML/CSS</h3>
+                  <div className='flex flex-wrap w-full justify-center gap-3 lg:w-auto lg:justify-start mt-6 mb-8'>
+                    <div className='badge'>UI/UX</div>
+                    <div className='badge'>ホームページ制作</div>
+                  </div>
+                  <p className='text-secondary'>Webサイトの開発を...</p>
+                </div>
+              </div>
+            </div>
           </section>
         </div>
       </div>
