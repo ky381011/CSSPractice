44c9650e3576c231c812eb132e10c800e27bac90
ky381011 <ky381011git@gmail.com>
2026-04-16 23:11:55 +0900

    feat: お問い合わせセクションにメールアドレスとメッセージ入力フィールドを追加し、送信ボタンを実装

--- practice/tailwind/portfolio01/src/App.tsx
+++ practice/tailwind/portfolio01/src/App.tsx
@@ -131,18 +131,29 @@ function App() {
         </div>
 
         {/* お問い合わせ */}
-        <div id="contact" className='container mt-64 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full z-10'>
+        <div id="contact" className='container mt-52 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full z-10'>
           <section className='w-full'>
             <h2 className='secondary-title'>
               お問い合わせ
             </h2>
             <p className='section-paragraph'>
-              私のサンプル連絡先です。
+              サンプルお問い合わせフォーマットです。
             </p>
             <div className='w-full grid lg:grid-cols-2 gap-8 lg:gap-32 mt-16'>
-              <div>
-                <label className='text-white block mb-6 text-xl font-bold'>お名前</label>
-                <input type="text" className='w-full border border-inputBorder bg-input px-4 py-4'></input>
+              <div className='space-y-12'>
+                <div>
+                  <label className='text-white block mb-6 text-xl font-bold'>お名前</label>
+                  <input type="text" className='w-full border border-inputBorder bg-input px-4 py-4'></input>
+                </div>
+                <div>
+                  <label className='text-white block mb-6 text-xl font-bold'>メールアドレス</label>
+                  <input type="text" className='w-full border border-inputBorder bg-input px-4 py-4'></input>
+                </div>
+                <div>
+                  <label className='text-white block mb-6 text-xl font-bold'>メッセージ</label>
+                  <input type="text" className='w-full border border-inputBorder bg-input px-4 py-4'></input>
+                </div>
+                <button className='px-8 py-3 mb-20 bg-theme font-bold rounded-lg hover:bg-purple-600 transition-all duration-300'>送信する</button>
               </div>
             </div>
           </section>
