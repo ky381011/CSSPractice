618cf06fe71771eaf075fe013e2b4679917eb261
ky381011 <ky381011git@gmail.com>
2026-04-23 23:42:08 +0900

    feat: PreviewFrameコンポーネントのスタイルを追加し、ウィジェットを更新

--- practice/animation/src/components/PreviewFrame.tsx
+++ practice/animation/src/components/PreviewFrame.tsx
@@ -1,11 +1,13 @@
+import './PreviewFrame.css'
+
 export function PreviewFrame(){
   return (
     <>
-      <div className="w-full h-full grid grid-cols-1 grid-rows-4 md:grid-cols-2 md:grid-rows-2">
-        <div className="border border-gray-400"></div>
-        <div className="border border-gray-400"></div>
-        <div className="border border-gray-400"></div>
-        <div className="border border-gray-400"></div>
+      <div className="w-full h-full grid grid-cols-1 grid-rows-4 sm:grid-cols-2 sm:grid-rows-2 overflow-hidden">
+        <div className="widget"></div>
+        <div className="widget"></div>
+        <div className="widget"></div>
+        <div className="widget"></div>
       </div>
     </>
   )
