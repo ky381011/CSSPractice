4b8ef3e4016819cd8537dda182772119789dc208
ky381011 <ky381011git@gmail.com>
2026-05-14 20:38:07 +0900

    feat: ClickPingコンポーネントを追加し、'Next'を'Click'に変更

--- practice/animation/src/components/LearningSets/LearningSet02_Ping.tsx
+++ practice/animation/src/components/LearningSets/LearningSet02_Ping.tsx
@@ -1,5 +1,20 @@
 import React from 'react'
 
+function ClickPing() {
+  const [pinging, setPinging] = React.useState(false)
+  return (
+    <div
+      className="relative flex h-full w-full items-center justify-center cursor-pointer"
+      onClick={() => setPinging(true)}
+    >
+      <div
+        className={`h-12 w-12 rounded-full bg-amber-400 ${pinging ? 'animate-ping' : ''}`}
+        onAnimationIteration={() => setPinging(false)}
+      ></div>
+    </div>
+  )
+}
+
 export const setName = 'Ping'
 
 export function getLearning02(): { name: string; element: React.ReactNode }[] {
@@ -32,12 +47,8 @@ export function getLearning02(): { name: string; element: React.ReactNode }[] {
       ),
     },
     {
-      name: 'Next',
-      element: (
-        <div className="relative flex h-full w-full items-center justify-center">
-          <div className='h-2 w-2 animate-ping rounded-full bg-amber-400'></div>
-        </div>
-      ),
+      name: 'Click',
+      element: <ClickPing />,
     },
   ]
 }
