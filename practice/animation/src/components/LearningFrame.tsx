41dff9e270cca75dc18a683eab1c9ca0c4a83145
ky381011 <ky381011git@gmail.com>
2026-05-15 22:45:58 +0900

    feat: LearningFrameにPulse学習セットを追加

--- practice/animation/src/components/LearningFrame.tsx
+++ practice/animation/src/components/LearningFrame.tsx
@@ -1,9 +1,10 @@
 import './PreviewFrame.css'
 import { getLearning01 } from './LearningSets/LearningSet01_Spin'
 import { getLearning02 } from './LearningSets/LearningSet02_Ping'
+import { getLearning03 } from './LearningSets/LearningSet03_Pulse'
 import { getLearningXX } from './LearningSets/LearningSetXX_Template'
 
-const learningSets = [getLearning01, getLearning02, getLearningXX]
+const learningSets = [getLearning01, getLearning02, getLearning03, getLearningXX]
 
 interface LearningFrameProps {
   setIndex: number
